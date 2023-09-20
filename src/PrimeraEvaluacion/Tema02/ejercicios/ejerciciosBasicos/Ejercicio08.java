f9f8f0b0b88be98a8748533e720097b1aabfbd57
anaijg <ana.jimenezgomez@educa.madrid.org>
2023-09-20 19:36:58 +0200

    Ejercicios 3 a 8

---
+++ src/PrimeraEvaluacion/Tema02/ejercicios/ejerciciosBasicos/Ejercicio08.java
@@ -0,0 +1,17 @@
+package PrimeraEvaluacion.Tema02.ejercicios.ejerciciosBasicos;
+/*
+Imagina que eres un gerente de recursos humanos en un restaurante y necesitas contratar a un chef. Para hacer eso, necesitas recopilar algunos datos preliminares sobre los candidatos, concretamente 5 campos: nombre, edad, años de experiencia, preferencia de cocina. Escribe un programa que lea todas las palabras (o números) una a una e imprima lo siguiente: "El formulario de <NOMBRE>, de <EDAD> años y <AÑOS DE EXPERIENCIA> de experiencia, está completo. Nos comunicaremos con usted si necesitamos a alguien que cocine comida <PREFERENCIA DE COCINA>".
+Entrada:
+Eugenio
+33
+8
+vegetariana
+Salida:
+El formulario de Eugenio, de 33 años y 8 de experiencia, está completo. Nos nos comunicaremos con usted si necesitamos alguien que cocine comida
+
+ */
+public class Ejercicio08 {
+    public static void main(String[] args) {
+
+    }
+}
