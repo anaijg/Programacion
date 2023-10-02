ee12d4374eadb7ab72e28afb12e41b3dc1c1ecce
anaijg <ana.jimenezgomez@educa.madrid.org>
2023-10-02 17:31:16 +0200

    Clase del 2 de octubre

--- src/PrimeraEvaluacion/Tema02/ejemplos/operadores/OperadoresLógicos.java
+++ src/PrimeraEvaluacion/Tema02/ejemplos/operadores/OperadoresLógicos.java
@@ -15,23 +15,39 @@ public class OperadoresLógicos {
         variable = !variable;
         System.out.println(variable); // false
 
-        System.out.println("\n\n**** and ****");
+        System.out.println("\n\n**** and y or ****");
         // los operados binarios and, or y not
         boolean v1 = true;
         boolean v2 = true;
+
         boolean resultadoAnd = v1 && v2;
-        System.out.println(resultadoAnd); // true
+        boolean resultadoOr = v1 || v2;
+        System.out.println("true\ttrue"); // \t mete una tabulación en el String o cadena de caracteres
+        System.out.println("AND: " + resultadoAnd); // true
+        System.out.println("OR: " +resultadoOr); // true
+
         v2 = false;
+        System.out.println("\ntrue\tfalse");
         resultadoAnd = v1 && v2;
+        resultadoOr = v1 || v2;
         System.out.println(resultadoAnd); // false
+        System.out.println(resultadoOr); // true
+
         v1 = false;
         v2 = true;
+        System.out.println("\nfalse\ttrue");
         resultadoAnd = v1 && v2;
+        resultadoOr = v1 || v2;
         System.out.println(resultadoAnd); // false
+        System.out.println(resultadoOr);
+
         v1 = false;
         v2 = false;
+        System.out.println("\nfalse\tfalse");
         resultadoAnd = v1 && v2;
+        resultadoOr = v1 || v2;
         System.out.println(resultadoAnd); // false
+        System.out.println(resultadoOr);
 
 
     }
