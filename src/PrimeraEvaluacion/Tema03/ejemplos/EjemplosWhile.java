a703ce835b2db9746a361ececbffef2188313a98
anaijg <ana.jimenezgomez@educa.madrid.org>
2023-10-19 17:13:23 +0200

    Ejercicio 28, ejemplos while y ejercicio 32

---
+++ src/PrimeraEvaluacion/Tema03/ejemplos/EjemplosWhile.java
@@ -0,0 +1,36 @@
+package PrimeraEvaluacion.Tema03.ejemplos;
+
+import java.util.Scanner;
+
+public class EjemplosWhile {
+    public static void main(String[] args) {
+        // el bucle while empieza evaluando una condición
+        // si el resultado es true, entra en el bloque {} y ejecuta el código que contenga
+        // al finalizar, vuelve arriba, a comprobar de nuevo la condición
+        // así, sucesivamente, hasta que la condición da como resultado false
+        int i = 0;
+        while (i < 11) {
+            System.out.println(i);
+            // para que esto pare en algún momento, tenemos que hacer que cambie la condición
+            i++;
+        }
+
+        // Otro ejemplo
+        // Imprimir todos los múltiplos de 5 hasta un número introducido por teclado
+        // Ejercicio 28 pero en vez de for con while
+        // NOTA: un buen entrenamiento puede ser hacer los ejercios de for con while
+        Scanner teclado = new Scanner(System.in);
+        int n = teclado.nextInt();
+        // reutilizo la i que tenía ya declarada para recoger el número por teclado
+        // tenemos que inicializar la i
+        i = 1;
+        while (i <= n) {
+            // si i es múltiplo de 5 (al dividirlo por 5, el resto da 0, lo  imprimo
+            if (i % 5 == 0) {
+                System.out.print(i + " ");
+            }
+            i++;
+        }
+
+    }
+}
---
+++ src/PrimeraEvaluacion/Tema03/ejercicios/Ejercicio32.java
@@ -0,0 +1,23 @@
+package PrimeraEvaluacion.Tema03.ejercicios;
+
+import java.util.Scanner;
+
+/*
+Escribe un programa que solicite al usuario un número entre 1 y 100, ambos inclusive. Si se introduce un número fuera del intervalo debe indicarlo mediante un error y seguir pidiendo el número hasta que se introduzca el número en el rango indicado. Al terminar debe imprimir por pantalla el número introducido.
+ */
+public class Ejercicio32 {
+    public static void main(String[] args) {
+        //solicite al usuario un número entre 1 y 100, ambos inclusive.
+        System.out.println("Introduce un número entre 1 y 100 (ambos inclusive):");
+        Scanner teclado = new Scanner(System.in);
+        int numero = teclado.nextInt();
+        // Si se introduce un número fuera del intervalo debe indicarlo mediante un error
+        while (numero < 1 || numero > 100) {
+            System.out.println("El número está fuera del rango.\nPor favor, introduce un número entre 1 y 100");
+            numero = teclado.nextInt(); // esto viene a ser como la i++, pero adaptándola a este ejercicio
+        }
+        // y seguir pidiendo el número hasta que se introduzca el número en el rango indicado. -> while
+        // Al terminar debe imprimir por pantalla el número introducido.
+        System.out.println(numero);
+    }
+}
