aff2614917724cdf0a12999cd7af2dd3ec1d8442
anaijg <ana.jimenezgomez@educa.madrid.org>
2023-11-09 16:53:56 +0100

    ibibuiob

--- src/PrimeraEvaluacion/Tema04/ejemplos/ComprobarOrdenAscendente.java
+++ src/PrimeraEvaluacion/Tema04/ejemplos/ComprobarOrdenAscendente.java
@@ -4,17 +4,10 @@ import java.util.Arrays;
 
 public class ComprobarOrdenAscendente {
     public static void main(String[] args) {
-        int[] array1 = new int[5];
-        for (int i = 0; i < array1.length; i++) {
-            if (i % 2 == 0) {
-                array1[i] = i + 2 * i;
-            } else {
-                array1[i] = i - 2 * i;
-            }
-        }
-        System.out.println(Arrays.toString(array1));
 
-        int[] array2 = {-5, 10, 16, 18, 100};
+        int[] array1 = {0, 1, 6, -3, 12}; // desordenado
+        int[] array2 = {-5, 10, 16, 18, 100}; // orden ascendente
+        int[] array3 = {8, 6, 3, -1, -5}; // orden descendente
 
         // imaginemos que no sabemos lo que contiene el array
         // queremos averiguar si está ordenado de forma ascendente
@@ -22,23 +15,19 @@ public class ComprobarOrdenAscendente {
         // truco: si está ordenado ascendentemente, cada elemento tiene que ser menor o igual que el siguiente
         // y tendría que recorrer todo el array comparando los elementos dos a dos
         // es más corto pensar así: si no está ordenado, basta que un elemento sea mayor que el siguiente
-        for (int i = 0; i < 5; i++) {
-            if (array1[i] > array1[i + 1]) { // con una vez que se cumpla esto, ya está desordenado
+        for (int i = 0; i < 4; i++) { // si no pongo como límite la penúltima posición (4), se desbordará cuando llegue a i + 1
+            if (array3[i] > array3[i + 1]) { // con una vez que se cumpla esto, ya está desordenado
                 ordenAscendente = false;
                 break; // nos saca fuera de for
             }
         }
         System.out.println("Array 1: " + ordenAscendente);
 
-        // comprobamos el segundo array
-        ordenAscendente = true;
-        for (int i = 0; i < 4; i++) { // si el array está ordenado, para no salirnos con i+1 tenemos que quedarnos en el penúltimo índice
-            if (array2[i] > array2[i + 1]) { // con una vez que se cumpla esto, ya está desordenado
-                ordenAscendente = false;
-                break; // nos saca fuera de for
-            }
+        if (ordenAscendente) {
+            System.out.println("El array está ordenado de forma ascendente");
         }
-        System.out.println("Array 2: " + ordenAscendente);
+
+        // hasta aquí hemos comprobado si está en orden ascendente
 
         //
     }
