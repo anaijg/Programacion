e9f55665a8e9f2c034a0f41c63eb645417ce076a
anaijg <ana.jimenezgomez@educa.madrid.org>
2023-11-09 17:05:32 +0100

    ibibuiob

--- src/PrimeraEvaluacion/Tema04/ejemplos/ComprobarOrdenAscendente.java
+++ src/PrimeraEvaluacion/Tema04/ejemplos/ComprobarOrdenAscendente.java
@@ -16,19 +16,32 @@ public class ComprobarOrdenAscendente {
         // y tendría que recorrer todo el array comparando los elementos dos a dos
         // es más corto pensar así: si no está ordenado, basta que un elemento sea mayor que el siguiente
         for (int i = 0; i < 4; i++) { // si no pongo como límite la penúltima posición (4), se desbordará cuando llegue a i + 1
-            if (array3[i] > array3[i + 1]) { // con una vez que se cumpla esto, ya está desordenado
+            if (array1[i] > array1[i + 1]) { // con una vez que se cumpla esto, ya está desordenado
                 ordenAscendente = false;
                 break; // nos saca fuera de for
             }
         }
-        System.out.println("Array 1: " + ordenAscendente);
+        //System.out.println("Array 1: " + ordenAscendente);
 
         if (ordenAscendente) {
             System.out.println("El array está ordenado de forma ascendente");
+        } else { // si no está en orden ascendente pueden pasar dos cosas: que esté en orden descendente o que esté desordenado
+            // comprobamos si está en orden descendente
+            boolean ordenDescendente = true;
+            for (int i = 0; i < array1.length - 1; i++) {
+                // basta con que un elemento sea menor que el siguiente para que no esté en orden
+                if (array1[i] < array1[i + 1]) {
+                    ordenDescendente = false; // y no haría falta comprobar más
+                    break; // saltamos fuera del bucle
+                }
+            }
+            if (ordenDescendente) {
+                System.out.println("El array está ordenado de forma descendente.");
+            } else {
+                // si no está en orden ascendente, ni en orden descendente, es que está desordenado
+                System.out.println("El array está desordenado.");
+            }
         }
 
-        // hasta aquí hemos comprobado si está en orden ascendente
-
-        //
     }
 }
