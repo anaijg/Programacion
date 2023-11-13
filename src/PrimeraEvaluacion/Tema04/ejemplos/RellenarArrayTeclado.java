5d17ce053b23d03e3b8d9a7cd236c9775ffd3f02
anaijg <ana.jimenezgomez@educa.madrid.org>
2023-11-13 17:39:24 +0100

    ibibuiob

---
+++ src/PrimeraEvaluacion/Tema04/ejemplos/RellenarArrayTeclado.java
@@ -0,0 +1,25 @@
+package PrimeraEvaluacion.Tema04.ejemplos;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RellenarArrayTeclado {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        // tenemos que declarar un array y asignarle un tamaño
+        // vamos a meter el tamaño también por teclado
+        int n = teclado.nextInt();
+        teclado.nextLine(); // para que no "salte" a la hora de meter luego un teclado.nextLine()
+        // suponemos que queremos rellenar el array con cadenas de texto
+        String[] cadenas = new String[n];
+
+        // ahora con un for vamos rellenándolo
+        for (int i = 0; i < cadenas.length; i++) { // también la condición podría ser i < n
+            // en el elemento con índice i de cadenas guardamos lo que entre por teclado
+            // no es necesario crear una variable aparte
+            cadenas[i] = teclado.nextLine();
+        }
+
+        System.out.println(Arrays.toString(cadenas));
+    }
+}
---
+++ src/PrimeraEvaluacion/Tema04/ejercicios/arrays/Ejercicio06.java
@@ -0,0 +1,11 @@
+package PrimeraEvaluacion.Tema04.ejercicios.arrays;
+/*
+Realizar un programa en el se se inicialice un array de longitud 20
+ con números aleatorios comprendidos entre -10 y 10.
+ A continuación, mostrar la media de los números positivos,la media de los negativos y contar el número de ceros.
+ */
+public class Ejercicio06 {
+    public static void main(String[] args) {
+
+    }
+}
