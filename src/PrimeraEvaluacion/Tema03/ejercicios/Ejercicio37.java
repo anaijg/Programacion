5ab61c857ba234a35acaacbe42ae51b8634a6be0
anaijg <ana.jimenezgomez@educa.madrid.org>
2023-11-20 18:05:05 +0100

    Ejercicios 9, 31, 37 tema 3 y 8 de String

---
+++ src/PrimeraEvaluacion/Tema03/ejercicios/Ejercicio37.java
@@ -0,0 +1,20 @@
+package PrimeraEvaluacion.Tema03.ejercicios;
+/*
+Hacer un programa que imprima por pantalla los 50 primeros múltiplos de 5, añadiendo un salto de línea cada 10 números.
+ */
+public class Ejercicio37 {
+    public static void main(String[] args) {
+        int contador = 0; // llevamos la cuenta de los múltiplos de 5 ; cuando llegue a 50, me paro
+        int i = 0; // variable del bucle, se va incrementando con cada vuelta
+        while (contador < 50) {
+            i++;
+            if (i % 5 == 0) { // si pasa esto, tenemos un múltiplo
+                contador++;
+                System.out.print(i + "\t");
+                if (contador % 10 == 0) { // tengo que meter un salto de línea cada 10 (en contador = 10, = 20, = 30, ...
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
