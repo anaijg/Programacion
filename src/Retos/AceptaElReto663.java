23943127f8acd42e4874b3513a900d047e0c32e9
anaijg <ana.jimenezgomez@educa.madrid.org>
2023-11-27 18:21:01 +0100

    Ejercicios 39 y 40 arrays

--- src/Retos/AceptaElReto663.java
+++ src/Retos/AceptaElReto663.java
@@ -7,7 +7,8 @@ Entrada
 La entrada comienza con un número indicando cuántos casos de prueba deberán ser procesados. Cada uno es un número entre −3.000 y 3000 representando un año. Los valores negativos indican años antes de Cristo y los positivos después de Cristo (o de la "era común"). Al no haber existido, en la entrada no habrá nunca un 0.
 
 Salida
-Por cada caso de prueba, el programa escribirá el año correspondiente en un supuesto calendario en el que sí existió el año 0, es decir un calendario donde el año 1 habría sido el año 0, manteniendo los anteriores igual.
+Por cada caso de
+prueba, el programa escribirá el año correspondiente en un supuesto calendario en el que sí existió el año 0, es decir un calendario donde el año 1 habría sido el año 0, manteniendo los anteriores igual.
 
 Entrada de ejemplo
 3
