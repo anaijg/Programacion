d054238a30f53bf50f9c855ae226c7df3ea3e12d
anaijg <ana.jimenezgomez@educa.madrid.org>
2024-01-10 19:28:29 +0100

    Ejercicio 16 arrays con arraylist

--- src/segundaevaluacion/poo/metodos/Ejercicio16ArraysConArrayList.java
+++ src/segundaevaluacion/poo/metodos/Ejercicio16ArraysConArrayList.java
@@ -8,64 +8,109 @@ public class Ejercicio16ArraysConArrayList {
     // quiero un Scanner que sea visible en todos los métodos de esta clase
     static Scanner teclado = new Scanner(System.in);
     public static void main(String[] args) {
-        //Crear un programa que cree un array de números enteros (tamaño introducido por teclado), muestre el siguiente menú y realice lo que en las opciones se indica:
+        //Crear un programa que cree un array de números enteros (tamaño introducido por teclado),
+        // muestre el siguiente menú y realice lo que en las opciones se indica:
          // otra forma: int tamaño = Integer.parseInt(teclado.nextline());
-        int tamaño = pedirNumero("Introduce el tamaño"); // me da igual porque va a ser un arraylist
+        //int tamaño = pedirNumero("Introduce el tamaño"); // me da igual porque va a ser un arraylis
+
 
         // crear un arraylist de números enteros
         ArrayList<Integer> numeros = crearArrayList();
 
-        //Añadir número al final: pide un número y lo añade al final del array.
-        // para tener un poco más de juego, metemos 5 números en el arraylist
-        for (int i = 0; i < 5; i++) {
-            numeros = añadirAlFinal(numeros);
-        }
+        //****** MENU *******
+        // variables que voy a utilizar en el switch
+        int opcion = 0;
+        int indice = 0;
+        int numero = 0;
+        do {
+            System.out.println("1. Añadir al final.");
+            System.out.println("2. Mostrar números.");
+            System.out.println("3. Añadir número en una posición.");
+            System.out.println("4. Longitud del array.");
+            System.out.println("5. Eliminar el último número.");
+            System.out.println("6. Eliminar un número.");
+            System.out.println("7. Contar números.");
+            System.out.println("8. Posiciones de un número.");
+            System.out.println("9. Salir.");
+            System.out.println("============================");
+            opcion = pedirNumero("Selecciona una opción (1 - 9): ");
+            System.out.println(opcion);
+            switch (opcion) {
+                case 1:
+                    //Añadir número al final: pide un número y lo añade al final del array.
+                    // para tener un poco más de juego, metemos 5 números en el arraylist
+                    for (int i = 0; i < 5; i++) {
+                        numeros = añadirAlFinal(numeros);
+                    }
+                    break;
+                case 2:
+                    // imprimimos el arraylist
+                    imprimirNumeros(numeros);
+                    break;
+                case 3:
+                    //Añadir número en una posición: pide un número y una posición, y si la posición existe en el array lo añade en esa posición (la posición se pide a partir de 1).
+                    numero = pedirNumero("Introduce número");
+                    indice = pedirNumero("¿En qué indice quieres que lo ponga?");
+                    if(indice <= numeros.size() - 1 && indice >= 0) {
+                        numeros.add(indice,numero);
+                    }
+                    break;
+                case 4:
+                    //Longitud del array: muestra el tamaño del array.
+                    System.out.println("Tamaño del array: " + numeros.size());
+                    break;
+                case 5:
+                    //Eliminar el último número: muestra el último número del array y lo borra.
+                    eliminarUltimo(numeros);
+                    break;
+                case 6:
+                    //Eliminar un número: pide una posición, y si la posición existe en el array borra el elemento que contiene (lo reemplaza con un 0).
+                    eliminarNumero(numeros);
+                    break;
+                case 7:
+                    //Contar números: pide un número y te dice cuántas veces aparece en el array.
+                    numero = pedirNumero("Dime el número que quieres contar");
+                    int contador = 0;
+                    for (Integer elemento: numeros) {
+                        if (elemento == numero) {
+                            contador++;
+                        }
+                    }
+                    System.out.println("El número " + numero + " aparece " + contador + " veces.");
+                    break;
+                case 8:
+                    //Posiciones de un número: pide un número y te dice en que posiciones está.
+                    ArrayList<Integer> posiciones = new ArrayList<>();
+                    numero = pedirNumero("Dime el número que quieres buscar: ");
+                    for (int i = 0; i < numeros.size(); i++) {
+                        if (numeros.get(i) == numero) {
+                            posiciones.add(i);
+                        }
+                    }
+                    System.out.println("El número " + numero + " aparece en las posiciones " + indice);
+                    break;
+                case 9:
+                    break;
+                default:
+                    System.out.println("Opción incorrecta.");
+            }
 
-        // imprimimos el arraylist
-        imprimirNumeros(numeros);
+        } while (opcion != 9);
 
-        //Añadir número en una posición: pide un número y una posición, y si la posición existe en el array lo añade en esa posición (la posición se pide a partir de 1).
-        int numero = pedirNumero("Introduce número");
-        int indice = pedirNumero("¿En qué indice quieres que lo ponga?");
-        if(indice <= numeros.size() - 1 && indice >= 0) {
-            numeros.add(indice,numero);
-        }
-        imprimirNumeros(numeros);
+    }
 
-        //Longitud del array: muestra el tamaño del array.
-        System.out.println("Tamaño del array: " + numeros.size());
-
-        //Eliminar el último número: muestra el último número del array y lo borra.
-        System.out.println("Se va a borrar el elemento: " + numeros.get(numeros.size() - 1));
-        numeros.remove(numeros.size() - 1);
-        System.out.println(numeros);
-        //Eliminar un número: pide una posición, y si la posición existe en el array borra el elemento que contiene (lo reemplaza con un 0).
-        indice = pedirNumero("Dime el índice del elemento que quieres borrar: ");
+    public static void eliminarNumero(ArrayList<Integer> numeros) {
+        int indice = pedirNumero("Dime el índice del elemento que quieres borrar: ");
         if (indice <= numeros.size() - 1) {
             numeros.remove(indice);
         }
         System.out.println(numeros);
-        //Contar números: pide un número y te dice cuántas veces aparece en el array.
-        numero = pedirNumero("Dime el número que quieres contar");
-        int contador = 0;
-        for (Integer elemento: numeros) {
-            if (elemento == numero) {
-                contador++;
-            }
-        }
-        System.out.println("El número " + numero + " aparece " + contador + " veces.");
-        //Posiciones de un número: pide un número y te dice en que posiciones está.
-        ArrayList<Integer> posiciones = new ArrayList<>();
-        numero = pedirNumero("Dime el número que quieres buscar: ");
-        for (int i = 0; i < numeros.size(); i++) {
-            if (numeros.get(i) == numero) {
-                posiciones.add(i);
-            }
-        }
-        System.out.println("El número " + numero + " aparece en las posiciones " + indice);
-        //Mostrar números: Muestra los números que contiene.
+    }
+
+    public static void eliminarUltimo(ArrayList<Integer> numeros) {
+        System.out.println("Se va a borrar el elemento: " + numeros.get(numeros.size() - 1));
+        numeros.remove(numeros.size() - 1);
         System.out.println(numeros);
-        //Salir
     }
 
     public static void imprimirNumeros(ArrayList<Integer> numeros) {
