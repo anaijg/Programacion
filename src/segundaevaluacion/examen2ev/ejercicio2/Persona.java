62cfdf054d2554638d86d0c4791370179399a7ed
anaijg <ana.jimenezgomez@educa.madrid.org>
2024-03-11 16:32:53 +0100

    Solución examen 2ª evaluación

---
+++ src/segundaevaluacion/examen2ev/ejercicio2/Persona.java
@@ -0,0 +1,22 @@
+package segundaevaluacion.examen2ev.ejercicio2;
+
+import java.time.LocalDate;
+
+public abstract class Persona {
+    protected String nombre;
+    protected String apellido1;
+    protected String apellido2;
+    protected LocalDate fechaNacimiento;
+
+    public Persona(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public abstract String getNombreCompleto();
+
+    public abstract void setNombreCompleto(String nombreCompleto);
+
+    public void cambiarFechaNacimiento(int dd, int mm, int aaaa) {
+        this.fechaNacimiento = LocalDate.of(aaaa, mm, dd);
+    }
+}
