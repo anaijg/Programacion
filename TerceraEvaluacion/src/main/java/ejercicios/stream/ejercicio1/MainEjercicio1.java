c2e541099c0e062bdd6512989ddecc071f8bb960
anaijg <ana.jimenezgomez@educa.madrid.org>
2024-04-12 20:36:01 +0200

    '12-04-2024'

---
+++ TerceraEvaluacion/src/main/java/ejercicios/stream/ejercicio1/MainEjercicio1.java
@@ -0,0 +1,42 @@
+package ejercicios.stream.ejercicio1;
+
+public class MainEjercicio1 {
+    public static void main(String[] args) {
+        // Crea una lista de enteros con 20 elementos aleatorios entre 1 y 15.
+
+
+
+        // Utilizando streams, haz lo siguiente:
+
+
+        // Imprime todos los elementos de la lista, sin ordenar.
+        System.out.println("1. Imprime todos los elementos de la lista, sin ordenar.");
+
+
+        //Imprime todos los elementos de la lista, ordenados.
+
+
+        //Imprime todos los elementos de la lista, sin repetir, ordenados.
+
+
+        //Imprime sólo los mayores o iguales a 5.
+
+
+
+        //Imprime sólo los mayores o iguales a 5, ordenados.
+
+
+
+        //Imprime sólo los mayuores o iguales a 5, ordenados inversamente.
+
+
+        //Suma todos los elementos de la lista.
+
+
+        //Suma todos los elementos mayores o iguales a 5.
+
+
+
+        //Calcula el promedio de todos los elementos de la lista.
+    }
+}
