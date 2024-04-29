9c56b50be8f7ab1f3829a36a8a51d1399f77ea3f
anaijg <ana.jimenezgomez@educa.madrid.org>
2024-04-29 18:23:02 +0200

    '29-04-2024'

---
+++ TerceraEvaluacion/src/main/java/ejercicios/ficheros/detexto/ejercicio5/Ejercicio5.java
@@ -0,0 +1,7 @@
+package ejercicios.ficheros.detexto.ejercicio5;
+/*
+Escribe un programa en Java que encuentre el número más grande en el fichero dataset_91007.txt
+ */
+public class Ejercicio5 {
+
+}
---
+++ TerceraEvaluacion/src/main/java/ejercicios/ficheros/detexto/ejercicio6/Ejercicio6.java
@@ -0,0 +1,6 @@
+package ejercicios.ficheros.detexto.ejercicio6;
+/*
+Escribir un programa que lea el contenido del fichero br.txt y muestre la información que almacena, de forma que muestre en lineas diferentes los fragmentos separados por un asterisco.
+ */
+public class Ejercicio6 {
+}
